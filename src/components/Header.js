99d909ab7ed475ee3cb9d50fb7c7f1772a79062b
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-03-20 02:41:30 +0530

    [UPDATE] Added NavBr Setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ const Header = (props) => {
             <Logo>
                 <img src="/images/logo.svg" alt="Disney+" />
             </Logo>
+            <NavMenu>menu</NavMenu>
         </Nav>
     )
 }
@@ -32,12 +33,28 @@ const Logo = styled.a`
     max-heigth: 70px;
     font-size: 0;
     display: inline-block;
-    
+
     img {
         display: block;
         width: 100%;
     }
 `;
 
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    justify-content: flex-end;
+    margin: 0px;
+    padding: 0px;
+    position: relative;
+    margin-right: auto;
+    margin-left: 25px;
+
+    @media (max-width) {
+        display: none;  
+    }
+`;
 
 export default Header;
