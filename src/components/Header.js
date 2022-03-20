1a02af2ad923d8e42298c32bbd79d41496ce4b06
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-03-20 20:58:25 +0530

    [UPDATE] Remaining Navbar items in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,26 @@ const Header = (props) => {
                     <img src='images/home-icon.svg' alt='HOME' />
                     <span>HOME</span>
                 </a>
+                <a>
+                    <img src='images/search-icon.svg' alt='SEARCH' />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src='images/watchlist-icon.svg' alt='WATCHLIST' />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src='images/original-icon.svg' alt='ORIGINALS' />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src='images/original-icon.svg' alt='MOVIES' />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src='images/original-icon.svg' alt='SERIES' />
+                    <span>SERIES</span>
+                </a>
             </NavMenu>
         </Nav>
     )
