aba89ea5176f5d796950912ee4987bd3f4f4bcc9
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-03-24 01:01:09 +0530

    [UPDATE] Added all Movie Category

--- src/components/Recommends.js
+++ src/components/Recommends.js
@@ -20,7 +20,7 @@ const Recommend = (props) => {
                      movies.map((movie, key) => (
                         <Wrap key={key}>
                             {movie.id}
-                            <Link to={'/detial/' + movie.id}>
+                            <Link to={'/detail/' + movie.id}>
                                 <img src={movie.cardImg} alt={movie.title} />
                             </Link>
                         </Wrap>
